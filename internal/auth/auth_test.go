5ca1b02ba0ff3345cdd4ac96c5a8be7d36329c08
sebastiantare <sebastiantarebustos@gmail.com>
2024-06-09 12:14:56 -0400

    added tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+  hh := http.Header{"Authorization": {"test"}}
+	got, err := GetAPIKey(hh)
+  print(err)
+	want := []string{"a"}
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
